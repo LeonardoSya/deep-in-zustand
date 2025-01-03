42bc5190d00ff6dcb0c2e14f2900c3f129c4ea37
LeonardoSya <2246866774@qq.com>
2025-01-03 11:05:53 +0800

    use zustand

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,30 +1,25 @@
-import { create, StateCreator } from 'zustand'
+import { create } from "zustand";
 
-// 1. 定义 store 的类型
-interface CounterStore {
-  count: number
-  increment: () => void
-  decrement: () => void
-}
+type Store = {
+  count: number;
+  add: () => void;
+  reduce: () => void;
+};
 
-// 2. 创建 store
-const useStore = create<CounterStore>((set: (fn: (state: CounterStore) => CounterStore) => void) => ({
-  count: 0,
-  increment: () => set((state: CounterStore) => ({ ...state, count: state.count + 1 })),
-  decrement: () => set((state: CounterStore) => ({ ...state, count: state.count - 1 }))
-}))
+const useStore = create<Store>()((set) => ({
+  count: 1,
+  add: () => set((state) => ({ count: state.count + 1 })),
+  reduce: () => set((state) => ({ count: state.count - 1 })),
+}));
 
-// 3. 在组件中使用 store
-function App() {
-  const { count, increment, decrement } = useStore()
+export default function App() {
+  const { count, add, reduce } = useStore();
 
   return (
-    <div>
-      <h1>计数器: {count}</h1>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-    </div>
-  )
+    <>
+      <div>count: {count}</div>
+      <button onClick={() => add()}>+</button>
+      <button onClick={() => reduce()}>-</button>
+    </>
+  );
 }
-
-export default App
---
+++ demo/src/storage.ts
@@ -0,0 +1,18 @@
+// const StorageKeys = {
+//   SESSION_TOKEN: "SESSION_TOKEN",
+//   USER_SETTINGS: "USER_SETTINGS",
+//   THEME: "THEME",
+// } as const;
+
+// export type StorageKeys = (typeof StorageKeys)[keyof typeof StorageKeys];
+
+// export class Storage {
+//   // 存储
+//   static setItem<T>(key: StorageKeys, value: T): void {
+//     const jsonValue = JSON.stringify(value);
+//     localStorage.setItem(key, jsonValue);
+//   }
+
+//   // 获取数据
+//   static getItem<T>
+// }
